5df9a99e5d63dac2ed828da1b6857548a63ab16b
Mishaelh <mishael.hernandez@outlook.com>
2023-04-24 12:35:35 +0900

    Final grade calculator has been set

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your final grade? ");
+        string gradeInput = Console.ReadLine();
+
+        int grade = int.Parse(gradeInput);
+
+        string finalGrade = "";
+
+        if (grade >= 90)
+        {
+            finalGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            finalGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            finalGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            finalGrade = "D";
+        }
+        else
+        {
+            finalGrade = "F";
+        }
+
+        Console.WriteLine($"Your final grade is {finalGrade}.");
+
+        if (grade >= 0)
+        {
+            if (grade>= 70)
+            {
+                Console.WriteLine("Congratulations you have PASSED the course!");
+            }
+            else
+            {
+                Console.WriteLine("Remember that with persistence comes sucess. Please try again!");
+            }
+        }
+
     }
 }
