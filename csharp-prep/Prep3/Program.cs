98ad969c9fb8c43219472f20678567414fbe2faa
Mishaelh <mishael.hernandez@outlook.com>
2023-04-27 17:26:45 +0900

    created guessing number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("Guess the magic number! ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber < guess)
+            {
+                Console.WriteLine("So close but guess lower!");
+            }
+
+            else if (magicNumber > guess)
+            {
+                Console.WriteLine("Keep trying that`s not the right number guess higher!");
+            }
+            else
+            {
+                Console.WriteLine("You have asserted. Congratulations!");
+            }
+        }                  
     }
 }
